49e9667074721131d818bd043ac080215dc187ca
EnriqueAP6 <Enrique.Albalate@alu.uclm.es>
2023-10-24 10:34:26 +0200

    Volcado de raw data en bbdd

--- Webscrapping/Scripts/Scraping_CO2.py
+++ Webscrapping/Scripts/Scraping_CO2.py
@@ -3,6 +3,7 @@ import csv
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from bs4 import BeautifulSoup as bs
+import pandas as pd
 
 def main():
     url="https://edgar.jrc.ec.europa.eu/booklet/EDGARv7.0_FT2021_fossil_CO2_booklet_2022.xlsx"
@@ -13,7 +14,10 @@ def main():
 def imprimirxlsx(data):
     with open('../DataSets/[RAW]data_CO2.xlsx', 'wb') as xlsx:
         xlsx.write(data)
+    df = pd.read_excel('../DataSets/[RAW]data_CO2.xlsx', sheet_name="fossil_CO2_totals_by_country")
+    df.to_csv("../DataSets/[RAW]data_CO2.csv", index=False) 
+    
 
 if __name__ == '__main__':
-    print("Executing Scrapping_Diesel_Gasoline.py")
+    print("Executing Scrapping_CO2.py")
     main()
--- Webscrapping/Scripts/Scraping_PIB_per_capita.py
+++ Webscrapping/Scripts/Scraping_PIB_per_capita.py
@@ -5,6 +5,17 @@ import pandas as pd
 import xlrd
 import os
 import numpy as np
+from sklearn.impute import SimpleImputer
+
+from pandas.io import sql
+import sqlalchemy as db
+from datetime import datetime,date
+from scipy import stats
+import pymysql
+
+
+
+
 '''
 import matplotlib.pyplot as plt
 import matplotlib as mpl
@@ -45,6 +56,7 @@ def obtener_datos(nombre_archivo_excel, nombre_archivo_csv):
     dataframe = pd.read_excel(workbook)
     dataframe.to_csv(nombre_archivo_csv, index=False)
     os.remove(nombre_archivo_excel)
+    return dataframe
 
 
 def filtrar_datos(nombre_archivo_csv):
@@ -52,42 +64,110 @@ def filtrar_datos(nombre_archivo_csv):
     dataframe_desde_2000 = dataframe_completo.iloc[1:dataframe_completo.shape[0]-2,[0] + list(range(21, dataframe_completo.shape[1]))]
     return dataframe_desde_2000
 
-
+'''
 def obtener_media_fila_sin_nulos(dataframe,num_fila): #obtengo la media de cada fila sin valores "no data"
     
         fila = dataframe.iloc[num_fila,1:] #quito la columna del pais
         contador_no_nulos = 0
         suma = 0
         for i in range(len(fila)):
-            if not fila[i] == 'no data':
+            if not pd.isna(fila[i]):
                 suma += float(fila[i])
                 contador_no_nulos += 1
 
         return suma/contador_no_nulos
+
+def formatear_valores_nulos(dataframe, valor_cambiar):
+    columnas_originales = dataframe.columns
+    imp = SimpleImputer(missing_values=valor_cambiar, strategy='constant', fill_value=np.nan)
+    dataframe= imp.fit_transform(dataframe)
+    dataframe = pd.DataFrame(dataframe, columns= columnas_originales)
+    return dataframe
+
 
 def limpiar_datos(dataframe):  #cambio de valores nulos por la media de cada fila
         
     for fila in range(dataframe.shape[0]):
         media_fila = obtener_media_fila_sin_nulos(dataframe, fila)
         for columna in range(dataframe.shape[1]):
-            if dataframe.iloc[fila,columna] == 'no data':
-                a =1
-                #dataframe[fila,columna] = media_fila
+            if pd.isna(dataframe.iloc[fila,columna]):
+                dataframe.iloc[fila,columna] = media_fila
                 
     return dataframe
+'''
 
+def anula_valores_no_numericos(dataframe):
+    return dataframe.apply(pd.to_numeric, errors='coerce')
+
+def media_no_nulos_fila(fila):
+    return pd.to_numeric(fila.dropna()).mean()
+
+def elimina_nulos(dataframe):
+    for index, fila in dataframe.iterrows():
+        dataframe.iloc[index-1] = fila.fillna(media_no_nulos_fila(fila))
+    return dataframe
+
+
+###################################################
+# PARA MYSQL
+###################################################
+
+def crea_traspuesta(dataframe):
+    traspuesta = datos_raw.iloc[:,1:].T.reset_index()
+    columnas_nuevas = pd.concat([pd.Series(['año']), datos_raw.iloc[:,0]])
+    traspuesta.columns = columnas_nuevas.str.replace(' ', '_').str.replace('[^a-zA-Z0-9_]', '')
+
+    traspuesta = traspuesta.iloc[:,[0]+list(range(2,traspuesta.shape[1]-2))]
+    return traspuesta
+
+def conecta_BD(dataframe, nombre_tabla):
+    database_username = "root"
+    database_password = "010203En"
+    database_ip = 'localhost'
+    database_name = "prueba"
+    database_connection = db.create_engine('mysql+pymysql://{0}:{1}@{2}/{3}'.
+                                           format(database_username,database_password,database_ip,database_name))
+    connection = database_connection.connect()
+    metadata = db.MetaData()
+    dataframe.to_sql(nombre_tabla, connection,if_exists='replace', index=False)
+
+# hacer una lambda función en la que se consiga la media de una fila (coja sólo los valores no nulos, los meta en 
+# una lista y luego devuelva la media)
+#hacer una lambda función que, dado un valor nulo, lo sustituya por el valor de la media de su fila calculada 
+# con la función anterior
+
+# sustituir cada nan por un 0 y con apply() hacer sum() de toda la fila con axis=1 y despues dividir esa cantidad
+# entre el numero de elementos que no sean 0 (Eso puede hacerse con otra función). Después habrá que sustituir
+# los 0 por cada valor correspondiente
 
 
 if __name__ == '__main__':
     print("Executing Scraping_PIB_per_capita.py")
     nombre_archivo_excel = "../DataSets/pib_per_capita.xls"
     nombre_archivo_csv = "../DataSets/[RAW]pib_per_capita.csv"
-    obtener_datos(nombre_archivo_excel, nombre_archivo_csv)
-    dataframe_desde_2000 = filtrar_datos(nombre_archivo_csv)
+    datos_raw = obtener_datos(nombre_archivo_excel, nombre_archivo_csv)
+    
+    '''dataframe_desde_2000 = filtrar_datos(nombre_archivo_csv)
+    print(dataframe_desde_2000)
+    columna_paises = dataframe_desde_2000.iloc[:,0]
+    dataframe_nulos_introducidos = anula_valores_no_numericos(dataframe_desde_2000.iloc[:,1:])
+    print(dataframe_nulos_introducidos)
+    dataframe_limpio = elimina_nulos(dataframe_nulos_introducidos)
+    print(pd.concat([columna_paises, dataframe_limpio], axis=1))'''
 
-    dataframe_sin_nulos = limpiar_datos(dataframe_desde_2000)
+    #conecta_BD(datos_raw,'pib_per_capita')
+    #conecta_BD(crea_traspuesta(datos_raw),'pib_per_capita_traspuesta')
     
-    
+
+    '''
+    dataframe_nulos_formateados = formatear_valores_nulos(dataframe_desde_2000,"no data")
+    dataframe_sin_nulos = limpiar_datos(dataframe_nulos_formateados)
+    #print(dataframe_sin_nulos)
+    dataframe_sin_paises = dataframe_nulos_formateados = dataframe_sin_nulos.iloc[:,1:]
+    print(dataframe_sin_paises)
+    print(dataframe_sin_paises.dtypes)
+    '''
+
 
         
         
