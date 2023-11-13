989907fdb7db9242cf9d1afc4f1e541e383d9a44
Mohamed Essalhi <mohamed.essalhi@alu.uclm.es>
2023-11-13 22:55:12 +0100

    Archivos CSV subidos a la BBDD

--- Preprocesado de Datos/[RAW]/[RAW]BBDD.py
+++ Preprocesado de Datos/[RAW]/[RAW]BBDD.py
@@ -1,52 +1,59 @@
-
-import sqlalchemy
-from sqlalchemy import create_engine, Column, Integer, String, MetaData, Table
-from sqlalchemy.orm import sessionmaker
 import pandas as pd
-import pymysql
-from pandas.io import sql
-import mysql.connector
+from sqlalchemy import create_engine, URL
+import warnings
 
-
-
-def conecta_BD(db_dict, url):
-    database_connection = sqlalchemy.create_engine(url)
-    connection = database_connection.connect()
-
-    for clave in db_dict.keys():
-        print('UPLOADING: ' + str(clave))
-        if "UTF" in clave:
-                dataframe_aux = pd.read_csv(db_dict[clave],encoding='utf-16')
-        else:
-                dataframe_aux = pd.read_csv(db_dict[clave])
-        dataframe_aux.columns = dataframe_aux.columns.str.replace(' ', '_').str.replace('[^a-zA-Z0-9_]', '')
-        dataframe_aux.to_sql(clave, connection,if_exists='replace', index=False)
-        
-
-
-# Datos de conexión a la base
-db_host = "db.programadormanchego.es"
-db_port = 3306
-db_user = "minero"
-with open("Preprocesado de Datos/Acceso BBDD/password.txt", 'r') as archivo_password:
-        db_password = archivo_password.read()
-db_name = "raw"
-ssl_ca = "Preprocesado de Datos/Acceso BBDD/ca.pem"
-db_dict = {
+warnings.filterwarnings('ignore')
+def DatosDataSets():
+    db_dict = {
            "data_CO2":"DataSets/[RAW]CO2_data.csv",
            "electric_car_data_clean":"DataSets/[RAW]electric_car_data_clean.csv",
-           #"electricity_all_countries":"DataSets/[RAW]electricity_in_all_countries.csv",
-           "gasoline_Diesel_pricesUTF":"DataSets/[RAW]gasoline_Diesel_prices.csv",
+           "electricity_all_countries":"DataSets/[RAW]electricity_in_all_countries.csv",
+           "gasoline_Diesel_prices":"DataSets/[RAW]gasoline_Diesel_prices.csv",
            "level_of_studies":"DataSets/[RAW]level_of_studies.csv",
            "model_per_year":"DataSets/[RAW]model_per_year.csv",
            "pib_per_capita":"DataSets/[RAW]pib_per_capita.csv",
            "puntos_de_carga":"DataSets/[RAW]puntos_de_carga.csv"
            }
+    return db_dict
 
-# Crear una conexión a la base de datos usando SQLAlchemy
-db_url = f"mysql+mysqlconnector://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}?ssl_ca={ssl_ca}"
+def DatosBBDD():
+    db_url = URL.create(
+        "mysql+pymysql",
+        username="minero",
+        password="MineriaMultiagentes2324*",
+        host="db.programadormanchego.es",
+        port=3306,
+        database="raw",
+    )
+    return db_url
 
-conecta_BD(db_dict, db_url)
+def ConectarseABBDD(local):
+    db_url = DatosBBDD()
+    if local:
+        connect_args = {"ssl": {"fake_flag_to_enable_tls": True}}
+        conn = create_engine(db_url, connect_args=connect_args)
+    else:
+        ssl_args = {"ssl_ca": "Preprocesado de Datos/Acceso BBDD/ca.pem", "ssl_verify_identity": False, "ssl_verify_cert": True}
+        conn = create_engine(db_url, connect_args=ssl_args)
+    conn.connect()
+    return conn
+
+def SubirArchivosABBDD(local):
+    conn = ConectarseABBDD(local)
+    datasets = DatosDataSets()
+    for nombre_dataset, path in datasets.items():
+        print("UPLOADING DATASET: " + str(nombre_dataset))
+        if nombre_dataset == 'gasoline_Diesel_prices':
+            df = pd.read_csv(path,encoding='utf-16')
+        elif nombre_dataset == 'level_of_studies':
+                df = pd.read_csv(path, on_bad_lines='skip')
+        elif nombre_dataset == 'model_per_year':
+               df= pd.read_csv(path, sep=';')
+        else:
+            df = pd.read_csv(path)            
+        df.columns = df.columns.str.replace(' ', '_').str.replace('[^a-zA-Z0-9_]', '')
+        df.to_sql(nombre_dataset, conn, if_exists="replace", index=False)
 
 
 
+SubirArchivosABBDD(local = False)
