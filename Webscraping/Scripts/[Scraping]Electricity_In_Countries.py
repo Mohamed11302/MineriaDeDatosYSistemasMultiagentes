d92c27ee658a7be40c4198ecf7cad93e3e9f767d
luciaancos <92110037+luciaancos@users.noreply.github.com>
2023-11-14 11:34:18 +0100

    Cambios en dataset de electricidad

--- Webscraping/Scripts/[Scraping]Electricity_In_Countries.py
+++ Webscraping/Scripts/[Scraping]Electricity_In_Countries.py
@@ -1,40 +1,19 @@
 import requests
-import io
-import zipfile
-import os
-import shutil
+import pandas
+from io import BytesIO
 
 def main():
 
-    url = "https://ember-climate.org/app/uploads/2022/09/european_wholesale_electricity_price_data_hourly-2.zip"
-
-    csv_folder = "DataSets/[RAW]electricity_in_countries_csv"
-    os.makedirs(csv_folder, exist_ok=True)
+    url = "https://ember-climate.org/app/uploads/2022/09/european_wholesale_electricity_price_data_monthly-5.csv"
 
     r = requests.get(url)
 
-    with zipfile.ZipFile(io.BytesIO(r.content), "r") as zip_zip:
-        
-        for file in zip_zip.infolist():
-            #Just .csv files saved
-            if file.filename.endswith(".csv"):
-                csv_content = zip_zip.read(file.filename)
-                
-                #If there are folders inside another folder
-                sub_dir = os.path.dirname(file.filename)
-                if sub_dir:
-                    os.makedirs(os.path.join(csv_folder, sub_dir), exist_ok=True)
-                
-                with open(os.path.join(csv_folder, file.filename), "wb") as csv_file:
-                    csv_file.write(csv_content)
+    df = pandas.read_csv(BytesIO(r.content))
 
-    #Quitar los archivos que no utilizaremos 
-    destino = 'DataSets/[RAW]electricity_in_all_countries.csv'
-    if os.path.exists(destino):
-        os.remove(destino)
-    os.rename(csv_folder +str("/european_wholesale_electricity_price_data_hourly/all_countries.csv"), destino)
-    shutil.rmtree(csv_folder)
-    
-if __name__ == "__main__":
-    print("Executing [Scraping]Electricity_In_Countries.py")
+    print(df)
+
+    df.to_csv("DataSets/[RAW]electricity_in_all_countries.csv", index=False)
+
+
+if __name__ == '__main__':
     main()
